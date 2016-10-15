09742cfd9f0e28af3671f211713aaad7bc9cb662
rstm.sf <rstm.sf@yandex.ru>
2016-10-15 12:56:01 +0500

    Добавлена функция

--- main.c
+++ main.c
@@ -3,8 +3,16 @@
 #include <inttypes.h>
 #include <math.h>
 
-uint16_t main() {
+double fun(double x);
 
+uint16_t main() {
+	double x = 1.0;
+	double tmp = 0.5 * x*x - 2.0 * x + 2.0;
+	printf("%f = %f ?", tmp, fun(x));
 
 	return 0;
+}
+
+double fun(const double x) {
+	return x < 1.0 ? 1.0 / (sin(x)*sin(x) + 1.0) : 0.5 * x*x - 2.0 * x + 2.0;
 }
